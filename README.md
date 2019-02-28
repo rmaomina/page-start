4a7404a97d026ea26f0251b75e5ee6123aa7d68e
rmaomina <rmaomina@gmail.com>
2019-02-28 12:14:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+## 프로젝트 시작을 위한 가이드 
+
+- https://github.com/rmaomina/StartProject.git
+- v1.1 | 20180402 | 프롬페이지 수정 
+- v2 | 20180724 | 프로젝트 시작을 위한 가이드로 수정 
+
+***
+
+## Directory
+
+- root
+	- index.html
+	- *assets*
+		- *css*
+			- *fonts*
+			- style.css 
+			- common.css
+			- typo.css
+			- modal.css
+			- layout.css
+		- *js*
+			- jquery.min.js
+			- common.js
+		- *img*
+			- *icons*
+			- *logo*
+			- *spot*
+	- *vendor*
+		- *datepickk*
+	- *comp*
+
+***
+
+## Media Query
+
+![mediaquery.png](http://rmao.dothome.co.kr/cloud/img/mediaquery.png)
+ - Mobile 320(아이폰5/5s) ~ 768
+ - Tablet 768 ~ 1024
+ - PC 1024 ~ 1280(1400)
+
+***
+
+## Markup Guide
+
+- [GitHub - lezhin/markup-guide: 레진 마크업(HTML/CSS) 가이드라인](https://github.com/lezhin/markup-guide/)
+
+## Check List
+[ ] 깃 등록 
+[ ] 공통요소 컴포넌트
+[ ] 라이브러리 
---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function(){
+    //modal
+    function showModal(modal) {
+        $('.modal' + modal).addClass('active');
+        $('.modal' + modal).addClass('active');
+        // console.log("success");
+    }
+
+    function hideModal(modal) {
+        $('.modal' + modal ).removeClass('active');
+    }
+    
+    //popupFixed
+    $('a.btn-modal-1').on('click', function(e) {
+        e.preventDefault();
+        showModal('#modalFixed');
+        $('body').addClass('fixed');
+    });
+
+    $('#modalFixed a.btn-modal-close').on('click', function(e) {
+        e.preventDefault();
+        hideModal('#modalFixed');
+        $('body').removeClass('fixed');
+    });
+
+    //popupLayered
+    $('a.btn-modal-2').on('click', function(e) {
+        e.preventDefault();
+        showModal('#modalLayer');
+    });
+
+    $('#modalLayer a.btn-modal-close').on('click', function(e) {
+        e.preventDefault();
+        hideModal('#modalLayer');
+    });
+
+    //2depth-menu
+    $('.area-nav .nav-items>li').mouseover(function(){
+		$(this).children(".nav-sub-items").stop().slideDown(300);
+	});
+	$('.area-nav .nav-items>li').mouseleave(function(){
+		$(this).children(".nav-sub-items").stop().slideUp(300);
+    });
+    
+    //nav-mb-only
+    $('.mb-only a.icon-sr-only').on('click', function(e) {
+        e.preventDefault();
+        $(".area-nav-mobile").toggle("slide", { direction: "right" }, 300);
+    });
+    
+    $('.area-nav-mobile a.btn-nav-close').on('click', function(e) {
+        e.preventDefault();
+        $(".area-nav-mobile").toggle("slide", { direction: "right" }, 300);
+    });
+
+    //nav-tb-only
+    $('.tb-only a.icon-sr-only').on('click', function(e) {
+        e.preventDefault();
+        $(".area-nav-tablet").slideToggle();
+    });
+
+
+});
